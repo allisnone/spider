31d9633d06f2495520224f8f3f4433cb28d2a0a4
allisnone <zgx20022002@163.com>
2019-03-30 11:45:45 +0800

    log function

--- mylogger.py
+++ mylogger.py
@@ -5,21 +5,25 @@ import logging
 import logging.handlers
 import datetime
 
-logger = logging.getLogger('mylogger')
-logger.setLevel(logging.DEBUG)
+def initial_logger(logfile='all.log',errorfile='error.log',logname='mylogger'):
+    logger = logging.getLogger(logname)
+    logger.setLevel(logging.DEBUG)
+    
+    rf_handler = logging.handlers.TimedRotatingFileHandler(logfile, when='midnight', interval=1, backupCount=7, atTime=datetime.time(0, 0, 0, 0))
+    rf_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+    
+    f_handler = logging.FileHandler(errorfile)
+    f_handler.setLevel(logging.ERROR)
+    f_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(filename)s[:%(lineno)d] - %(message)s"))
+    
+    logger.addHandler(rf_handler)
+    logger.addHandler(f_handler)
+    
+    logger.debug('debug message')
+    logger.info('info message')
+    logger.warning('warning message')
+    logger.error('error message')
+    logger.critical('critical message')
+    return logger
 
-rf_handler = logging.handlers.TimedRotatingFileHandler('all.log', when='midnight', interval=1, backupCount=7, atTime=datetime.time(0, 0, 0, 0))
-rf_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
-
-f_handler = logging.FileHandler('error.log')
-f_handler.setLevel(logging.ERROR)
-f_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(filename)s[:%(lineno)d] - %(message)s"))
-
-logger.addHandler(rf_handler)
-logger.addHandler(f_handler)
-
-logger.debug('debug message')
-logger.info('info message')
-logger.warning('warning message')
-logger.error('error message')
-logger.critical('critical message')
+#logger = initial_logger(logfile='all.log',errorfile='error.log',logname='mylogger')
